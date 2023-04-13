ba4e733d8054fb7c9a3bca306232e26633c93812
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-13 19:35:03 +0530

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // css
 import NCss from "./Css/Nav.module.css";
 
 export default function Nav() {
-  return <div className={NCss.mDiv}>Nav</div>;
+  return (
+    <div className={NCss.mDiv}>
+      <Link to="/" className="LinkStyle">
+        Nav
+      </Link>
+      <Link to="/Login" className="LinkStyle">
+        Login/Register
+      </Link>
+    </div>
+  );
 }
