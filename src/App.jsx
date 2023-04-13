57446562981c60807840340dcd87c23239a36327
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-13 19:30:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+// Layout
+import Layout from "./Pages/Layout";
+
+// Fixed Components
+import Nav from "./Components/Nav";
+import Footer from "./Components/Footer";
+
+// Components
+import Home from "./Pages/Home";
+import Error from "./Pages/Error";
+import Register from "./Pages/Register";
+
 function App() {
   return (
     <BrowserRouter>
