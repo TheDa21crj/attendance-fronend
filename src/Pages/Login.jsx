9097c01b1c395617d68d2da1abfeeee31fb2e5bc
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-13 19:42:34 +0530

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+// Css
+import LCss from "./Css/Login.module.css";
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className={LCss.mDIv}>
+      <p>Login</p>
+      <input type="text" name="" id="" />
+      <input type="text" name="" id="" />
+    </div>
+  );
 }
