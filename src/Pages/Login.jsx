b5fff2baf198ccf5b4d15d7cfb2fdc775239e600
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-13 19:50:20 +0530

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,11 +4,49 @@ import React, { useState } from "react";
 import LCss from "./Css/Login.module.css";
 
 export default function Login() {
+  const [showUser, setUser] = useState({ email: "", password: "" });
+  const [showPassword, setPassword] = useState(false);
+
+  const DataInp = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setUser({ ...showUser, [name]: value });
+  };
+
+  const PostData = async () => {
+    const { email, password } = showUser;
+    console.log(email);
+    console.log(password);
+  };
   return (
     <div className={LCss.mDIv}>
       <p>Login</p>
-      <input type="email" name="" id="" placeholder="Email" />
-      <input type="password" name="" id="" placeholder="Password" />
+      <input
+        type="email"
+        name="email"
+        id=""
+        placeholder="Email"
+        onChange={DataInp}
+      />
+      <input
+        type={showPassword ? "text" : "password"}
+        name="password"
+        id=""
+        placeholder="Password"
+        onChange={DataInp}
+      />
+      <div
+        onClick={() => {
+          setPassword(!showPassword);
+        }}
+      >
+        {showPassword ? "Show Password" : "Hide Password"}
+      </div>
+
+      <div>
+        <button onClick={PostData}>Login</button>
+      </div>
     </div>
   );
 }
