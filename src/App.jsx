bf42abeaacc8ab92798d4d6a73101bc83d05e02e
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-13 19:23:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
