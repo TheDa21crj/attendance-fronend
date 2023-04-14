1008c38b1f2c13639ea1a46a80b75757cd246814
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-14 11:37:43 +0530

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,10 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+
+// redux
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { adduser } from "./../redux/user";
+import { toggleN } from "./../redux/toggleNav";
 
 // css
 import NCss from "./Css/Nav.module.css";
 
 export default function Nav() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    AuthMiddleware();
+  }, []);
+
+  const AuthMiddleware = async () => {
+    try {
+      const res = await fetch(
+        `${import.meta.env.VITE_Backend_Base}/api/user/account`,
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        }
+      );
+
+      const data = await res.json();
+
+      console.table(data);
+      if (data.errors) {
+        console.log("errors");
+        dispatch(toggleN({ toggle: "false" }));
+        return;
+      }
+      if (data) {
+        console.log("==============");
+        // setimg(data.message.avatar);
+        dispatch(
+          adduser({
+            _id: data.message._id,
+            email: data.message.email,
+            imgSrc: data.message.avatar,
+            // firstName: data.message.firstName,
+            // LastName: data.message.LastName,
+            // gender: data.message.gender,
+            // PhoneNumber: data.message.PhoneNumber,
+          })
+        );
+        // dispatch(toggleN({ toggle: "true" }));
+      }
+    } catch (error) {
+      dispatch(toggleN({ toggle: "false" }));
+      return;
+    }
+  };
   return (
     <div className={NCss.mDiv}>
       <Link to="/" className="LinkStyle">
