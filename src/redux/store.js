810d81582c599153995fdf92ef0ae765c56300cc
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-14 11:37:48 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import toggleReducer from "./toggleNav";
 import userData from "./user";
 
 export default configureStore({
   reducer: {
+    toggle: toggleReducer,
     user: userData,
   },
 });
