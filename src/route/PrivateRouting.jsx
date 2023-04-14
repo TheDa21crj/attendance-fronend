52450d829a577a627f1069c25e40ff28934ffac1
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-14 11:37:52 +0530

    Update PrivateRouting.jsx

--- src/route/PrivateRouting.jsx
+++ src/route/PrivateRouting.jsx
@@ -3,7 +3,9 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const PrivateRoute = () => {
+  console.log("first===");
   const email = useSelector((state) => state.user.email);
+  console.log(email);
   return email ? <Outlet /> : <Navigate to="/login" />;
 };
 
