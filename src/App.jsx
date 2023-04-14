06776cdead2559dee7c1a8938f677ef754ab7410
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-14 10:29:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,19 +14,30 @@ import Login from "./Pages/Login";
 import Error from "./Pages/Error";
 import Register from "./Pages/Register";
 
+// PrivateRouting
+import PrivateRoute from "./route/PrivateRouting";
+
+// redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-        <Footer />
-      </Layout>
+      <Provider store={store}>
+        <Layout>
+          <Nav />
+          <Routes>
+            <Route exact path="/" element={<PrivateRoute />}>
+              <Route path="/" element={<Home />} />
+            </Route>
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Register" element={<Register />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+          <Footer />
+        </Layout>
+      </Provider>
     </BrowserRouter>
   );
 }
