fc68cb28c144ca89a4e3458147bf98ca9a1607b0
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-26 21:39:13 +0530

    Update List.jsx

--- src/Components/Attendance/List.jsx
+++ src/Components/Attendance/List.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+// axios
+import axios from "axios";
 
 export default function List() {
+  const viewAtt = async () => {
+    try {
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    viewAtt();
+  }, []);
+
   return <div>List</div>;
 }
