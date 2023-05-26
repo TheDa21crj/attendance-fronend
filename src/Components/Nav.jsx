c3b113211431c1f23f40cdfc2477fae417371701
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-26 20:00:26 +0530

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+
+// state
+import AuthContext from "./store/auth-context";
 
 // css
 import NCss from "./Css/Nav.module.css";
 
 export default function Nav() {
+  const authCtx = useContext(AuthContext);
   return (
     <div className={NCss.mDiv}>
       <Link to="/" className="LinkStyle">
