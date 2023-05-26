aaaa651ff327af7ef19e39c01a3bc650bb89856b
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-26 21:39:16 +0530

    Update List.jsx

--- src/Components/Attendance/List.jsx
+++ src/Components/Attendance/List.jsx
@@ -6,6 +6,9 @@ import axios from "axios";
 export default function List() {
   const viewAtt = async () => {
     try {
+      const res = await axios.post("/api/attendance/View", userObject, {
+        headers: { Authorization: `` },
+      });
     } catch (error) {}
   };
 
