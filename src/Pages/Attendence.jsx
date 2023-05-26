0232cbc0b8827834f313a83126b8c387d32accf7
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-26 21:19:25 +0530

    Update Attendence.jsx

--- src/Pages/Attendence.jsx
+++ src/Pages/Attendence.jsx
@@ -2,14 +2,18 @@ import React from "react";
 
 // Components
 import Start from "./../Components/Attendance/Start";
+import List from "./../Components/Attendance/List";
 
 export default function Attendence() {
   return (
     <div>
+      <br />
+      <br />
       Attendence
       <br />
       <br />
       <Start />
+      <List />
     </div>
   );
 }
