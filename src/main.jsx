cdb55fbe752c4177b07716f16b5897e0ec0f8e8d
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-26 19:09:10 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { AuthContextProvider } from "./store/auth-context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <AuthContextProvider>
     <App />
-  </React.StrictMode>
+  </AuthContextProvider>
 );
