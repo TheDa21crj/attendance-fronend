cdf8bb6ca20d08032fcb6d8fb3e910d425f8c594
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-26 19:21:26 +0530

    Update Attendence.jsx

--- src/Pages/Attendence.jsx
+++ src/Pages/Attendence.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import AuthContext from "./../store/auth-context";
+import { useNavigate } from "react-router-dom";
 
 export default function Attendence() {
-  return <div>Attendence</div>;
+  const authCtx = useContext(AuthContext);
+  const redirect = useNavigate();
+
+  const logout = () => {
+    redirect("/Login");
+    authCtx.logout();
+  };
+  return (
+    <div>
+      Home
+      <br />
+      <br />
+      <br />
+      <p>Name = {authCtx.user.name}</p>
+      <p>Email = {authCtx.user.email}</p>
+      <br />
+      <br />
+      <p onClick={logout}>Logout</p>
+    </div>
+  );
 }
