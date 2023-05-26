1af57631fa075f5fd837948e4e1d9040b95ae882
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-26 20:05:35 +0530

    Create MyAccount.jsx

---
+++ src/Pages/MyAccount.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect, useContext } from "react";
+import AuthContext from "./../store/auth-context";
+import { useNavigate } from "react-router-dom";
+
+export default function Attendence() {
+  const authCtx = useContext(AuthContext);
+  const redirect = useNavigate();
+
+  const logout = () => {
+    redirect("/Login");
+    authCtx.logout();
+  };
+  return (
+    <div>
+      <br />
+      <br />
+      <p>Name = {authCtx.user.name}</p>
+      <p>Email = {authCtx.user.email}</p>
+      <br />
+      <br />
+      <p onClick={logout}>Logout</p>
+      <br />
+      <br />
+    </div>
+  );
+}
